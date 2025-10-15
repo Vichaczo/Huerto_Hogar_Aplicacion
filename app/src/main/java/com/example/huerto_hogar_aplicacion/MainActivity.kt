21546381761f0463d22d3c83fd0a3adc4994aae1
Vicente <vic.bravoc@duocuc.cl>
2025-10-15 16:42:16 -0300

    Se agrega proyecto a github

---
+++ app/src/main/java/com/example/huerto_hogar_aplicacion/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.huerto_hogar_aplicacion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.huerto_hogar_aplicacion.ui.theme.Huerto_Hogar_AplicacionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Huerto_Hogar_AplicacionTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Huerto_Hogar_AplicacionTheme {
+        Greeting("Android")
+    }
+}
